8bd46a693388ee2d0ee48e490478c8a7820f681f
Michael Andre <miketandre@gmail.com>
2016-04-04 07:39:07 -0500

    html templates and styles

--- js/app.js
+++ js/app.js
@@ -2,3 +2,28 @@ require("../css/styles.scss");
 
 window.$ = require("jquery");
 var _tpl = require("lodash/template");
+
+$(document).ready(function() {
+    render(data);
+});
+
+var render = function(data) {
+
+    if (data.length > 0) {
+        var wrapper = $('.wep--polls');
+
+        $.each(data,function(i,v) {
+            var categoryTemplateString = '';
+
+            $.each(v.categories,function(ind,val) {
+                var categoryTpl = _tpl($('#template--category').html());
+                var chtml = categoryTpl(val);
+                categoryTemplateString = categoryTemplateString + chtml;
+            });
+
+            var pollTpl = _tpl($('#template--poll').html());
+            var phtml = pollTpl({'topic': v.topic, "categorieshtml": categoryTemplateString});
+            wrapper.append(phtml);
+        });
+    }
+}
